749592f5f2f42c1cde0039098d78723318adfd0c
Guibibi <bgui34@gmail.com>
2023-03-01 01:37:58 -0500

    Init commit

---
+++ lua/custom/plugins/alpha.lua
@@ -0,0 +1,28 @@
+return {
+	'goolord/alpha-nvim',
+	lazy = false,
+	config = function()
+		local dashboard = require("alpha.themes.dashboard")
+
+		-- Set the header
+		dashboard.section.header.val = {
+			" _______  __   __  _______  _______  _______  __   __  ___   __   __ ",
+			"|       ||  | |  ||       ||       ||       ||  | |  ||   | |  |_|  |",
+			"|    ___||  |_|  ||   _   ||  _____||_     _||  |_|  ||   | |       |",
+			"|   | __ |       ||  | |  || |_____   |   |  |       ||   | |       |",
+			"|   ||  ||       ||  |_|  ||_____  |  |   |  |       ||   | |       |",
+			"|   |_| ||   _   ||       | _____| |  |   |   |     | |   | | ||_|| |",
+			"|_______||__| |__||_______||_______|  |___|    |___|  |___| |_|   |_|",
+		}
+		-- Set our menu
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("f", "  Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
+			dashboard.button("r", "  Recent", ":Telescope oldfiles<CR>"),
+			dashboard.button("s", "  Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+			dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
+		}
+
+		require 'alpha'.setup(dashboard.opts)
+	end
+}
