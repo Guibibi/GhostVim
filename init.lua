08cc31cc4859870a63fbb27f7370fe79b86186e3
Guibibi <bgui34@gmail.com>
2023-03-06 23:15:42 -0500

    Added gitsigns keybindings

--- init.lua
+++ init.lua
@@ -59,7 +59,9 @@ require('lazy').setup({
   { -- Adds git releated signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     keys = {
-      {"<leader>gb", "<cmd>Gitsigns blame_line<cr>", desc = "Git blame line"}
+      {"<leader>gb", "<cmd>Gitsigns blame_line<cr>", desc = "Git Blame Line"},
+      {"<leader>gd", "<cmd>Gitsigns toggle_deleted<cr>", desc = "Git Toggle Deleted"},
+      {"<leader>gt", "<cmd>Gitsigns diffthis<cr>", desc = "Git Diff This"},
     },
     opts = {
       -- See `:help gitsigns.txt`
