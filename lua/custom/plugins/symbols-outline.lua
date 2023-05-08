c6a317731e9fd28e8c7b7af00ff6d4324c5dddeb
Guillaume Bédard <bgui34@gmail.com>
2023-05-08 12:14:01 -0400

    Removed symbols-outline

--- lua/custom/plugins/symbols-outline.lua
+++
@@ -1,9 +0,0 @@
-return {
-	'simrat39/symbols-outline.nvim',
-	config = function()
-		require('symbols-outline').setup()
-	end,
-	keys = {
-		{"<leader>co", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline"}
-	}
-}
