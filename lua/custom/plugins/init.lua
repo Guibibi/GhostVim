4bf0f8af6424ed2d89f0fe3e60963df209d37d66
Guillaume Bedard <g.bedard@appcom.ca>
2024-10-15 16:20:06 -0400

    feat: Add Flash for quickly jumping around buffer

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,6 +3,7 @@
 --
 -- See the kickstart.nvim README for more information
 return {
+  -- ToggleTerm (Floating terminal)
   {
     'akinsho/toggleterm.nvim',
     config = function()
@@ -12,6 +13,7 @@ return {
       }
     end,
   },
+  --- Helpview (Better helpview)
   {
     'OXY2DEV/helpview.nvim',
     config = function()
@@ -26,6 +28,56 @@ return {
     lazy = true,
     keys = {
       { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
+  -- Flash (Jump around quickly in buffer)
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.config
+    opts = {},
+    --- stylua: ignore
+    keys = {
+      {
+        's',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').jump()
+        end,
+        desc = 'Flash',
+      },
+      {
+        'S',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').treesitter()
+        end,
+        desc = 'Flash Treesitter',
+      },
+      {
+        'r',
+        mode = 'o',
+        function()
+          require('flash').remote()
+        end,
+        desc = 'Remote Flash',
+      },
+      {
+        'R',
+        mode = { 'o', 'x' },
+        function()
+          require('flash').treesitter_search()
+        end,
+        desc = 'Treesitter Search',
+      },
+      {
+        '<c-s>',
+        mode = { 'c' },
+        function()
+          require('flash').toggle()
+        end,
+        desc = 'Toggle Flash Search',
+      },
     },
   },
 }
